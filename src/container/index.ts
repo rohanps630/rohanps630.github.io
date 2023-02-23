43ae9f0632a97d42a50e7c78515775b4b43a82b4
rohanps630 <rohanpsuresh@gmail.com>
2023-02-23 14:19:48 +0530

    Portfolio

--- src/container/index.ts
+++ src/container/index.ts
@@ -2,7 +2,7 @@ import Header from './Header/Header';
 import About from './About/About';
 import Work from './Work/Work';
 import Skills from './Skills/Skills';
-import Testimonial from './Testimonial/Testimonial';
+// import Testimonial from './Testimonial/Testimonial';
 import Footer from './Footer/Footer';
 
 export {
